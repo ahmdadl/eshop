997eb01297ccbc4428b4dd105d280f030925ab25
ahmed adel <abo3adel35@gmail.com>
2020-03-29 22:24:25 +0200

    remove ide helper doc block from model

--- app/Category.php
+++ app/Category.php
@@ -5,26 +5,6 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-/**
- * App\Category
- *
- * @property int $id
- * @property string $name
- * @property int|null $category_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Category[] $categories
- * @property-read int|null $categories_count
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Category newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Category newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Category query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Category whereCategoryId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Category whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Category whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Category whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Category whereUpdatedAt($value)
- * @mixin \Eloquent
- */
 class Category extends Model
 {
     public function categories() : HasMany
