dd38ef18b07a6028d3d1ae2e40fd48e4ee16309c
ahmed adel <abo3adel35@gmail.com>
2020-03-29 02:51:12 +0200

    add lang code to all routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeCookieRedirect', 'localeSessionRedirect']
+    ],
+    function () {
+        Route::get('/', function () {
+            return view('welcome');
+        });
 
-Auth::routes();
+        Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+        Route::get('/home', 'HomeController@index')->name('home');
+    }
+);
