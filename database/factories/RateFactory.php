4c0a69584828fc28d3734af78804dfec950a5f31
ahmed adel <abo3adel35@gmail.com>
2020-03-30 03:01:47 +0200

    create ratings table

---
+++ database/factories/RateFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Rate;
+use Faker\Generator as Faker;
+
+$factory->define(Rate::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
