753db896e57ff4b1dfa2b3020a8bee77875174e5
ahmed adel <abo3adel35@gmail.com>
2020-03-31 02:29:32 +0200

    seed ratings

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(ProductSeeder::class);
+        $this->call(RateSeeder::class);
     }
 }
