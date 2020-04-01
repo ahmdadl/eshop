eb7293b0acda1130dec8a1de0e4d91580edf47c9
ahmed adel <abo3adel35@gmail.com>
2020-04-01 03:31:25 +0200

    configure laravel localization so http request will work

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,21 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Mcamara\LaravelLocalization\LaravelLocalization;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public static function setUpBeforeClass(): void
+    {
+        putenv(LaravelLocalization::ENV_ROUTE_KEY . '=en');
+        parent::setUpBeforeClass();
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        putenv(LaravelLocalization::ENV_ROUTE_KEY);
+        parent::tearDownAfterClass();
+    }
 }
