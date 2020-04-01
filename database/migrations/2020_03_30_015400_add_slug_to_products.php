7c808e098f476152c2beb1e24ea9b97803638477
ahmed adel <abo3adel35@gmail.com>
2020-04-01 18:50:22 +0200

    slug is index for faster search times

--- database/migrations/2020_03_30_015400_add_slug_to_products.php
+++ database/migrations/2020_03_30_015400_add_slug_to_products.php
@@ -14,7 +14,7 @@ class AddSlugToProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('slug')->nullable();
+            $table->string('slug')->nullable()->index();
         });
     }
 
