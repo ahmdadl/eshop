e96b9c02597faa06de7cdcf450ff7620c47132ae
ahmed adel <abo3adel35@gmail.com>
2020-04-02 03:43:28 +0200

    show default images

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,32 +1,41 @@
-@extends('layouts.app', ['cpt' => 'home'])
+@extends('layouts.app', ['cpt' => 'product'])
 
 @section('title')
 @lang('t.all_p')
 @endsection
 
 @section('content')
-<div ref='product_data' class="row">
-    @foreach ($pros[0] as $p)
-    <div class="card col-6 col-sm-4 col-md-3 col-lg-2">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-        <h5 class="card-title">{{$p->name}}</h5>
-            <p class="card-text">
-                <strong class="text-primary">
-                    {{$p->price}}
-                </strong>
-            </p>
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Cras justo odio</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-        </ul>
-        <div class="card-body">
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
+<div class="row">
+    <div class="col-6 col-sm-4 col-md-3 col-lg-2" v-for="p in h.d.data">
+        <div class="card">
+            <img :src="'/img/' + p.img[0]" class="card-img-top" :alt="p.name +' image'">
+            <div class="card-body">
+                <p>
+                    <span class="badge badge-danger">
+                        @{{p.save}} % OFF
+                    </span>
+                </p>
+                <h5 class="card-title">@{{p.name}}</h5>
+                <p class="card-text">
+                    <strong class="text-primary">
+                        @{{p.savedPrice}} USD
+
+                        <span v-if="p.save"
+                            class="d-block text-muted text-dell">
+                            @{{p.price}} USD
+                        </span>
+
+                    </strong>
+                </p>
+                <p>
+                    @{{p.rateAvg}}
+                </p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
         </div>
     </div>
-    @endforeach
 </div>
 @endsection
