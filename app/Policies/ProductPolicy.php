2709a189bc8ed7387bbafb8fd6b62b1b4bfde77e
ahmed adel <abo3adel35@gmail.com>
2020-04-10 06:42:50 +0200

    add police to product

---
+++ app/Policies/ProductPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Product;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ProductPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any products.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        // return true;
+    }
+
+    /**
+     * Determine whether the user can view the product.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Product  $product
+     * @return mixed
+     */
+    public function view(User $user, Product $product)
+    {
+        // return true;
+    }
+
+    /**
+     * Determine whether the user can create products.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        // return true;
+    }
+
+    /**
+     * Determine whether the user can update the product.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Product  $product
+     * @return mixed
+     */
+    public function update(User $user, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the product.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Product  $product
+     * @return mixed
+     */
+    public function delete(User $user, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the product.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Product  $product
+     * @return mixed
+     */
+    public function restore(User $user, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the product.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Product  $product
+     * @return mixed
+     */
+    public function forceDelete(User $user, Product $product)
+    {
+        //
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Policies\ProductPolicy;
+use App\Product;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        Product::class => ProductPolicy::class,
     ];
 
     /**
