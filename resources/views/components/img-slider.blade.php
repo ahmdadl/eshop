afc921448ecf510b093f5dfaf52a6977372d55e2
ahmed adel <abo3adel35@gmail.com>
2020-04-11 00:22:24 +0200

    fix active slide

--- resources/views/components/img-slider.blade.php
+++ resources/views/components/img-slider.blade.php
@@ -1,30 +1,30 @@
 @props(['id', 'imgArr'])
 
 @php
-  $id = $id ?? bin2hex(random_bytes(5));
+$id = $id ?? bin2hex(random_bytes(5));
 @endphp
 
 <div id="carousel{{$id}}" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-        <li data-target="#carousel{{$id}}" data-slide-to="0"
-            class="active"></li>
+        <li data-target="#carousel{{$id}}" data-slide-to="0" class="active">
+        </li>
         <li data-target="#carousel{{$id}}" data-slide-to="1"></li>
         <li data-target="#carousel{{$id}}" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
         @foreach ($imgArr as $img)
-        <div class="carousel-item active">
-                <img src="/img/{{$img}}" class="d-block w-100" alt=".">
-            </div>
+        <div class="carousel-item {{$loop->index === 1 ? 'active' : ''}}">
+            <img src="/img/{{$img}}" class="d-block w-100" alt=".">
+        </div>
         @endforeach
     </div>
-    <a class="carousel-control-prev" href="#carousel{{$id}}"
-        role="button" data-slide="prev">
+    <a class="carousel-control-prev" href="#carousel{{$id}}" role="button"
+        data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carousel{{$id}}"
-        role="button" data-slide="next">
+    <a class="carousel-control-next" href="#carousel{{$id}}" role="button"
+        data-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
