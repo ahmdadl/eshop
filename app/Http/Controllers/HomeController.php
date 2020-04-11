96d96198bad13318a1482398c52df3773720f9a4
ahmed adel <abo3adel35@gmail.com>
2020-04-11 23:25:31 +0200

    show a product from every category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    use GetCategoryList;
+
     /**
      * Create a new controller instance.
      *
@@ -24,7 +26,7 @@ class HomeController extends Controller
     public function index()
     {
         return view('home', [
-            'cats' => Category::whereNull('category_id')->with('subCat')->get(),
+            'cats' => $this->getList(),
         ]);
     }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,56 @@
 @extends('layouts.app', ["cpt" => 'home'])
 
 @section('title')
-    EShop HomePage
+{{__('t.home.title')}}
 @endsection
 
 @section('content')
-{{-- <div v-if="h.allData">
-    <ul>
-        <li v-for="c in h.allData">
-            @{{c.name}}
-</li>
-</ul>
-<div v-if="!h.allData.length" class="alert alert-danger fade">
-    <strong>asdasd</strong>
+<div class="container-fluid">
+@foreach ($cats as $cat)
+<h3 class="mt-5 text-primary text-uppercase">
+    <strong>{{$cat->name}}:</strong>
+</h3>
+<div class="row row-cols-2 row-cols-sm-3 row-cols-md-6">
+    @foreach ($cat->subCat as $c)
+    @php
+    $c->load(['productsMini'=>function($query) {
+    return $query->take(1);
+    }]);
+    @endphp
+    @foreach ($c->productsMini as $p)
+    <div class="card p-0 mt-3">
+        @if ($p->save)
+        <span class="position-absolute badge badge-danger p-2">
+            {{$p->save}} % @lang('t.offTxt')
+        </span>
+        @endif
+        <img src="/img/{{$cat->slug}}/{{$p->img[0]}}" class="card-img-top"
+            alt="{{$p->slug}}">
+        <div class="card-body">
+            <h5 class="card-title">
+                <a href="/{{app()->getLocale()}}/p/{{$p->slug}}">
+                    {{$p->name}}
+                </a>
+            </h5>
+            <p class="card-text text-primary">
+                <strong>
+                    ${{\number_format($p->savedPrice, 2)}}
+                </strong>
+                @if ($p->save)
+                <strong class="text-muted d-block">
+                    <del>${{\number_format($p->price, 2)}}</del>
+                </strong>
+                @endif
+            </p>
+            <p>
+                <star-rate :percent="{{$p->rateAvg}}"
+                    :count="{{$p->rates->count()}}"></star-rate>
+            </p>
+        </div>
+    </div>
+    @endforeach
+    @endforeach
 </div>
-<div v-if="h.allData.length" class="alert alert-sucess fade">
-    <strong>asdasd</strong>
+@endforeach
 </div>
-
-</div>
-@{{h.allData}} --}}
-{{-- <div v-if="h">
-    <h3>@{{h.name}}</h3>
-</div>
-<h1>@{{h.id}}</h1>--}}
-
-
-    <button v-on:click="h.d.log">asdasd</button><br>
-    <button v-on:click="h.d.name = 'rand' + Math.random()">changeName</button><br>
-    @{{h.d}}<br>
-    @{{h.d.name}}<br>
-    @{{h.d.test}}<br>
 @endsection
