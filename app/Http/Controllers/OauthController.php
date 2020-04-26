743293d4b31f81590a2dd77dbecde4ed1c51e861
ahmed adel <abo3adel35@gmail.com>
2020-04-26 05:36:49 +0200

    add user agent header

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -55,7 +55,8 @@ class OauthController extends Controller
         $opt = [
             'headers' => [
                 'Accept' => 'application/json',
-                'authorization' => "Bearer $token"
+                'authorization' => "Bearer $token",
+                'User-Agent'=> "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
             ]
         ];
 
